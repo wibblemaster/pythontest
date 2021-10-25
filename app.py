6344b1693d3f947f13741db905621be301983d7d
Stephen Gibbs <Stephen.Gibbs@Schroders.com>
2021-10-25 11:01:22 +0100

    Added another docstrung to satisfy linter

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 """Just a very simple http server - not for production use!"""
 
 def main():
+    """Main function to do something useful"""
     print("This is an example to show simple comments from python")
 
 if __name__ == "__main__":
