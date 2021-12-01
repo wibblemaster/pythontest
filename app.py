778f32e1f8e758bd36684cc513e2c39496b77669
Stephen Gibbs <Stephen.Gibbs@Schroders.com>
2021-12-01 15:04:21 +0000

    Added another_function

--- app.py
+++ app.py
@@ -3,6 +3,11 @@
 def main():
     """Main function to do something useful"""
     print("This is an example to show simple comments from python")
+    another_function()
+
+def another_function():
+    """Some other function that prints something profound"""
+    print("Example for printing")
 
 if __name__ == "__main__":
     main()
