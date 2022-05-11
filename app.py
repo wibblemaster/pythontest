f8badb2796a6eda6d0ed9072010432bdefda94ff
Stephen Gibbs <stephen_gibbs@hotmail.com>
2022-05-11 20:05:23 +0100

    Updates for some more changes

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def original_changes():
     """Some more original changes"""
     print("Added other changes")
 
+def some_other_changes():
+    """Some other changes"""
+    print("Some other stuff/changes")
+
 if __name__ == "__main__":
     main()
