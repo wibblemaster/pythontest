dc686c8c2a66e58ec3daa6f8680d59f43d8c2b36
Stephen Gibbs <stephen_gibbs@hotmail.com>
2023-01-14 15:05:54 +0000

    Some minor changes

--- app2.py
+++ app2.py
@@ -3,13 +3,12 @@
 app2 - this is some fancy module what I wrote
 """
 
-
 def main():
     """
     Usage: main
-    
     Does some spectacular main type thing in code
     """
+
     print("Does something")
 
 if __name__ == "__main__":
