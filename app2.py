84424573600a9fb52d7d435673313eadcf1d698c
Stephen Gibbs <stephen_gibbs@hotmail.com>
2023-01-14 14:51:52 +0000

    Added some docstring

--- app2.py
+++ app2.py
@@ -1,6 +1,11 @@
 #/usr/bin/env python
 
 def main():
+    """
+    Usage: main
+    
+    Does some spectacular main type thing in code
+    """
     print("Does something")
 
 if __name__ == "__main__":
